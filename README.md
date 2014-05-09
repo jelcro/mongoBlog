a6b73a709d267b79a59bb5eba25ea8f1992684ae
j <jelcro@gmail.com>
2014-05-08 22:00:31 -0500

    post and comment domain classes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Demo application showing how to use MongoDB to store user and role data for version 2.x of the Spring Security Core plugin.
+Made with Burt Beckwith's mongoSpringSecurity as a template
 
-See http://burtbeckwith.com/blog/?p=1992 for usage information.
+"Demo application showing how to use MongoDB to store user and role data for version 2.x of the Spring Security Core plugin.
+
+See http://burtbeckwith.com/blog/?p=1992 for usage information."
--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,8 +4,11 @@ import auth.User
 class BootStrap {
 
 	def init = { servletContext ->
-		def role = new Role(authority: 'ROLE_USER').save()
-		new User(username: 'admin', password: 'password',
-		         email: 'example@email.com', authorities: [role]).save(flush:true)
+		def roleAdmin = new Role(authority: 'ROLE_ADMIN').save()
+		def roleUser  = new Role(authority: 'ROLE_USER').save()
+		def roleDummy = new Role(authority: 'ROLE_DUMMY').save()
+		new User(username: 'example', password: 'password',
+		         email: 'example@email.com', authorities: [roleDummy]).save(flush:true)
 	}
+	
 }
